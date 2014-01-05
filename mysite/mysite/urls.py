10839dc29d3628619ab406a94508204d1e3c7801
zyz913614263 <913614263@qq.com>
2014-01-05 13:14:54 +0800

    Create urls.py

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/',include('blog.urls')),
+    url(r'^judge/',include('judge.urls')),
+)
